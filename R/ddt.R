528f342da42e3353a932849afe8debe09f05b165
aselvaag <alexis.r.selvaag-1@ou.edu>
2025-11-01 00:30:50 -0500

    added ddt data set and myboot2 function

---
+++ R/ddt.R
@@ -0,0 +1,16 @@
+#' DDT data set.
+#'
+#' A data set collected by the US forces from the tennessee river and its tributaries
+#'
+#' @format A data frame with 144 rows and 5 variables:
+#' \describe{
+#'   \item{RIVER}{river name}
+#'   \item{MILE}{mile marker}
+#'   \item{SPECIES}{name of species}
+#'   \item{LENGTH}{length in cm}
+#'   \item{WEIGHT}{weight of the fish, in gms}
+#'   \item{DDT}{DDT concentration in ppm}
+#'   ...
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"ddt"
