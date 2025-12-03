b1e30dd8f044c06035dadac53f4c0128ba8c7578
aselvaag <alexis.r.selvaag-1@ou.edu>
2025-12-03 17:48:15 -0600

    added various things to my package

--- R/ddt.R
+++ R/ddt.R
@@ -1,8 +1,8 @@
 #' DDT data set.
 #'
-#' A data set collected by the US forces from the tennessee river and its tributaries
+#' A data set collected by the US forces from the Tennessee river and its tributaries
 #'
-#' @format A data frame with 144 rows and 5 variables:
+#' @format A data frame with 144 rows and 6 variables:
 #' \describe{
 #'   \item{RIVER}{river name}
 #'   \item{MILE}{mile marker}
@@ -10,7 +10,6 @@
 #'   \item{LENGTH}{length in cm}
 #'   \item{WEIGHT}{weight of the fish, in gms}
 #'   \item{DDT}{DDT concentration in ppm}
-#'   ...
 #' }
 #' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
 "ddt"
---
+++ R/fire.R
@@ -0,0 +1,11 @@
+#' FIRE data set.
+#'
+#' A data set from MS that shows data from fire damage incidents.
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{distance from fire station to fire, in miles}
+#'   \item{DAMAGE}{Thousands of dollars damage}
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
